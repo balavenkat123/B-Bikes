ea82a31d8449238b0e45ee12d79a9e34d6432ad9
bala venkat <balavenkat17902@gmail.com>
2023-08-19 03:09:52 +0530

    constantsindexjs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
-import { bike1, bike2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, smallredbike,smallblackbike, thumbnailShoe3 } from "../assets/images";
+import { bike1, bike2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, smallredbike,smallblackbike,smallbluebike } from "../assets/images";
 
 export const navLinks = [
     { href: "#home", label: "Home" },
@@ -18,7 +18,7 @@ export const bikes = [
         bigShoe: bike2,
     },
     {
-        thumbnail: thumbnailShoe3,
+        thumbnail: smallbluebike,
         bigShoe: bigShoe3,
     },
 ];
