b0533078447965f4325f9f8819d7f4108e202f04
bala venkat <balavenkat17902@gmail.com>
2023-08-28 04:37:43 +0530

    productionssec1

--- src/sections/Popularbikes.jsx
+++ src/sections/Popularbikes.jsx
@@ -2,7 +2,11 @@ import { products } from "../constants"
 
 const Popularbikes = () => {
   return (
-    <section id="products" className="max-container"></section>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+
+      </div>
+    </section>
   )
 }
 
