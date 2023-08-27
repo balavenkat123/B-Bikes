38606ef74b810cdae4f5880c64d21e954a1cd474
bala venkat <balavenkat17902@gmail.com>
2023-08-28 04:43:05 +0530

    h2ofproducts

--- src/sections/Popularbikes.jsx
+++ src/sections/Popularbikes.jsx
@@ -4,7 +4,8 @@ const Popularbikes = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
       <div className="flex flex-col justify-start gap-5">
-
+        <h2 className="text-4xl font-bold"><span className="text-sky-500">Popular</span>  products</h2>
+        <p>here are some more products which can be provided</p>
       </div>
     </section>
   )
