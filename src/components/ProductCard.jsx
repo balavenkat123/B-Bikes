149080d680741e6c1159882f940a20c5cda65e9a
bala venkat <balavenkat17902@gmail.com>
2023-08-30 08:28:02 +0530

    productscart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
-const ProductCard = (imgURL,name,price) => {
+const ProductCard = ({imgURL,name,price}) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-        <img src={imgURL} alt="bike4" className="w-[280px] h-[280px]"/>
+        <img src={imgURL} alt={name} className="w-[280px] h-[280px] bg-blue-100"/>
     </div>
   )
 }
