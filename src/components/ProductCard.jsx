f2cf80f4028418218b165dcf3e8fb89c747e1e37
bala venkat <balavenkat17902@gmail.com>
2023-09-02 09:50:52 +0530

    popularproducts finished

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,11 +3,12 @@ import { star } from "../assets/icons"
 const ProductCard = ({imgURL,name,price}) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-        <img src={imgURL} alt={name} className="w-[280px] h-[280px] "/>
+        <img src={imgURL} alt={name} className="w-[280px] h-[280px] bg-sky-200 hover:bg-sky-500"/>
         <div className="mt-8 flex justifu-start gap-4">
-            <img src={star} alt="rating" width={24} height={24} />5
+           5 <img src={star} alt="rating" width={24} height={24} />
         </div>
-        <h3>{name}</h3>
+        <h3 className="text-2xl mt-2 font-semibold font-palanquin">{name}</h3>
+        <h3 className="text-xl text-red-500">{price}</h3>
     </div>
 
   )
